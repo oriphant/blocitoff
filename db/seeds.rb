0ae95d3f390d1a16041d8bf21a92d7fb175ec7be
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-11-12 00:51:10 -0800

    Created Faker Seed data, also update config/environments/development.rb file to allow localhost.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+users = User.all
+10.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10),
+    )
+    user.skip_confirmation!
+    user.save!
+end
+
+# Create an admin user
+admin = User.new(
+  name: 'Admin Man',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  )
+admin.skip_confirmation!
+admin.save!
+
+# Create To Do's
+items=Item.all
+50.times do
+  Item.create!(
+    name: Faker::Lorem.sentence,
+    user: users.sample,
+    created_at: Faker::Date.between(20.days.ago, Date.today) #Interesting to generate expired entries
+    # Can you use an IF statement in Faker -> If created_at +7.days THEN make status expired
+    )
+end
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Item.count} To Do's created"
+
